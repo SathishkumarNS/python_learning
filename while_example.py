fc588f8e6575f56945fd66611edf2207c39b7465
SathishkumarNS <118336251+SathishkumarNS@users.noreply.github.com>
2022-11-18 19:09:35 +0530

    Added while exercise

---
+++ while_example.py
@@ -0,0 +1,24 @@
+
+
+
+# Intialize name
+name = ["k", "a", "r", "t", "h", "i", "k"]
+
+# initialize array's index pointer to zero
+i = 0
+#in while check position is less than or equal to name array's length
+while i <= len(name) - 1 :
+      # print character at name index
+      print(name[i])
+      # increment index position
+      i = i + 1
+      
+
+
+
+
+
+
+
+
+
